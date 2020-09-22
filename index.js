ed24b19ca8c5564ff829d0f2f55a3b7de1793db7
ngschaider <gschaiderniklas@gmail.com>
2020-09-22 09:24:23 +0000

    Added CORS and allow timestamp input

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-const express = require("express")
-const app = express()
+const cors = require("cors");
+const express = require("express");
+const app = express();
 
 const PORT = process.env.PORT;
 
-app.get("/api/timestamp/:dateString", (req, res) => {
-  const dateString = req.params.dateString;
 
-  const date = new Date(dateString);
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+app.get("/api/timestamp/:date_string?", (req, res) => {
+  var dateString = req.params.date_string;
+
+  if(/\d{5,}/.test(dateString)) {
+    dateString = parseInt(dateString);
+  }
+
+  const date = dateString ? new Date(dateString) : new Date();
 
   if(date.toString() === "Invalid Date") {
     res.json({error: date.toString()});
